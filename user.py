70c199c2b6bf99d41f26052d6635181a0b1113f0
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-18 00:54:42 +0200

    added credential class with delete and save instances

--- user.py
+++ user.py
@@ -30,4 +30,29 @@ class User :
         '''
         User.user_list.remove(self)
 
+class Credentials :
+    '''
+    class to create credential instances
+    '''
+    creds_list = []
+    
+    def __init__(self, account, user_name, pass_word):
+        '''
+        method for credential properties
+        '''
+        self.account = account
+        self.user_name = user_name
+        self.pass_word = pass_word
+
+    def save_creds(self):
+        '''
+        method to save credentials in credential list
+        '''
+        Credentials.creds_list.append(self)
+
+    def delete_creds(self):
+        '''
+        method to remove credential info from our list
+        '''
+        Credentials.creds_list.remove(self)
         
