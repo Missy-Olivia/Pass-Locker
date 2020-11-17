4d1b86d6bae30eb2ec5cb995bba72b0782257a2d
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-17 14:05:45 +0200

    added init method with instance variables

--- user.py
+++ user.py
@@ -3,3 +3,10 @@ class User :
     class to generate new intances of a user
     '''
     user_list = []
+
+def __init__ (self,username,password):
+    '''
+    method that helps define the properties of our objects.
+    '''
+    self.username = username
+    self.password = password
