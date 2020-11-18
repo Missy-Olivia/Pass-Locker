ac9d922b146680c355953d3794d8bf0322034fd0
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-11-18 02:09:00 +0200

    ran save, save multiple and delete tests

--- test_user.py
+++ test_user.py
@@ -25,5 +25,33 @@ class TestUser (unittest.TestCase):
         self.assertEqual(self.new_user.username, "Missy")
         self.assertEqual(self.new_user.password, "xoxo")
 
+    def test_save_user(self):
+        '''
+        test verify whether user objects are saved into the  user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+    def test_save_multiple_users(self):
+        '''
+        testing to see if we can store manu user info in our list
+        '''
+        self.new_user.save_user()
+        test_user = User("Loud", "123")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    def test_delete_user(self):
+        '''
+        test to see if we can remove user from list
+        '''
+        self.new_user.save_user()
+        test_user = User("Loud", "123")
+        test_user.save_user()
+
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list),1)
+
+
 if __name__ == "__main__":
     unittest.main()
